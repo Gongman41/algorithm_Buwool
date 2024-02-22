e31373f70cda7dcaf12dbeec2a59e7f6ae8db742
Gongman41 <lkm98401@gmail.com>
2024-02-22 12:49:45 +0900

    이것저것 추가

--- BFS_DFS/2573_빙산/gong.py
+++ BFS_DFS/2573_빙산/gong.py
@@ -1,46 +1,45 @@
 import sys
-from collections import deque                 
+from collections import deque
 
-N,M = map(int,sys.stdin.readline().split())
-dr_r = [1,0,-1,0]
-dr_c = [0,-1,0,1]
-matrix = [list(map(int,sys.stdin.readline().split()))for _ in range(N)]
+# sys.stdin = open('input.txt')
+N, M = map(int, sys.stdin.readline().split())
+dr_r = [1, 0, -1, 0]
+dr_c = [0, -1, 0, 1]
+matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 year = 0
-cnt = 1
-viewed = [[0]*M for _ in range(N)]
+viewed = [[0] * M for _ in range(N)]
 while True:
-    
+
     cnt = 0
-    
+
     stack = deque()
-    for i in range(1,N-1):
-        for j in range(1,M-1):
+    for i in range(1, N - 1):
+        for j in range(1, M - 1):
             if matrix[i][j] > 0 and 0 <= viewed[i][j] < year + 1:
                 cnt += 1
-                
-                stack.append([i,j])
+                viewed[i][j] += 1
+
+                stack.append([i, j])
             while stack:
                 count = 0
                 star = stack.popleft()
-                viewed[star[0]][star[1]] += 1
-                
+
                 for n in range(4):
-                    if 0 <= star[0] + dr_r[n] < N and 0 <= star[1] +dr_c[n] < M and matrix[star[0] + dr_r[n]][star[1] + dr_c[n]] > 0 and 0 <=viewed[star[0] + dr_r[n]][star[1] + dr_c[n]] < year +1:
-                        
-                        stack.append([star[0] + dr_r[n],star[1] + dr_c[n]] )
-                        viewed[star[0]+dr_r[n]][star[1]+dr_c[n]] += 1
-                
-                    if 0 <= star[0] + dr_r[n] < N and 0 <= star[1] +dr_c[n] < M and matrix[star[0] + dr_r[n]][star[1] + dr_c[n]] <= 0 and 0<=viewed[star[0] + dr_r[n]][star[1] + dr_c[n]] < year +1:
+                    if 0 <= star[0] + dr_r[n] < N and 0 <= star[1] + dr_c[n] < M and matrix[star[0] + dr_r[n]][
+                        star[1] + dr_c[n]] > 0 and 0 <= viewed[star[0] + dr_r[n]][star[1] + dr_c[n]] < year + 1:
+                        stack.append([star[0] + dr_r[n], star[1] + dr_c[n]])
+                        viewed[star[0] + dr_r[n]][star[1] + dr_c[n]] += 1
+
+                    if 0 <= star[0] + dr_r[n] < N and 0 <= star[1] + dr_c[n] < M and matrix[star[0] + dr_r[n]][
+                        star[1] + dr_c[n]] <= 0 and 0 <= viewed[star[0] + dr_r[n]][star[1] + dr_c[n]] < year + 1:
                         count += 1
                 matrix[star[0]][star[1]] -= count
+
     if cnt == 1:
         year += 1
     else:
         break
-        
-                
-            
-    
+
 if cnt == 1:
     print(0)
 else:
