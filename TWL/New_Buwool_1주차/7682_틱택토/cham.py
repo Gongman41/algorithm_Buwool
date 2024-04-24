8eb16507e02e7c70ae0791935cf2546a3af500ca
차민주 <ckalswn007@naver.com>
2024-04-24 20:03:15 +0900

    아놔 잘못 올려서 다시 올립니뎅

--- TWL/New_Buwool_1주차/7682_틱택토/cham.py
+++ TWL/New_Buwool_1주차/7682_틱택토/cham.py
@@ -1,6 +1,33 @@
 import sys
 sys.stdin = open('input.txt')
 
+# X가 선공
+# 다 찼다면 X가 하나 더 많아야 함
+# o가 이겼으면 x 개수 = o 개수
+# x가 이겼으면 x 개수가 하나 더 많음
+
+win_idx = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+def win_x(x_data):
+    for win in win_idx:
+        if win in X_idx:
+            return True
+        else:
+            continue
+    else:
+        return False
+
+
+def win_o(o_data):
+    for win in win_idx:
+        if win in O_idx:
+            return True
+        else:
+            continue
+    else:
+        return False
+
+
 while True:
     board = input()
     if board == 'end':
@@ -11,13 +38,31 @@ while True:
         if board[i] == 'O':
             O_num += 1
             O_idx.append(i)
-        else:
+        elif board[i] == 'X':
             X_num += 1
             X_idx.append(i)
-    if abs(O_num - X_num) > 1:  # 각 팀의 말 개수 차이가 2 이상이면
+    # print(O_idx, X_idx)
+    # print(O_num, X_num)
+    if O_num < 3 and X_num < 3:
         print('invalid')
-        break
-    elif O_num < 3 and X_num < 3:   # 각 팀의 말 개수가 3개가 안되면
+    elif X_num == (O_num + 1):    # X가 이길 수 있음
+        if win_x(X_idx):    # 예상대로 x가 이기면
+            print('valid')
+        else:   # 어 근데 다 채워서 무승부인 경우도 있음 이거 처리
+            if win_o(O_idx):
+                print('invalid')
+            else:
+                print('valid')
+    elif X_num == O_num:  # O가 이길 수 있음
+        if win_o(O_idx):
+            print('valid')
+        else:
+            print('invalid')
+    elif X_num < O_num or (X_num - O_num) > 1:
         print('invalid')
-        break
+#
+# _XX
+# X_X
+# OOO
+
 
