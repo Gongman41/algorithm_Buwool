cac391f544cfd8a794da8073631521a985c53494
dhwan <ehdghks9719@naver.com>
2024-05-02 19:21:17 +0900

    ㅈㅅ

--- TWL/New_Buwool_2주차/1715_카드정렬하기/dong.py
+++ TWL/New_Buwool_2주차/1715_카드정렬하기/dong.py
@@ -1,2 +1,23 @@
 import sys
 sys.stdin = open('input.txt')
+from queue import PriorityQueue
+
+N = int(input())
+pq = PriorityQueue()
+
+for _ in range(N):
+    c = int(input())
+    pq.put(c)
+
+data1 = 0
+data2 = 0
+sumi = 0
+
+while pq.qsize()>1:
+    data1 = pq.get()
+    data2 = pq.get()
+    temp = data1+data2
+    sumi += temp
+    pq.put(temp)
+    
+print(sumi)    
